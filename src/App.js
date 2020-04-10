bb88da054a235c21433fc4f5c03915a5fed7078c
Ricardo Arellano <ricarellano@Ricardos-MacBook-Pro.local>
2020-04-09 22:41:58 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import NavBar from "./components/layout/NavBar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  };
+
+  // Sea rch Github Users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_GITHUB_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
+  render() {
+    const { users, loading } = this.state;
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const NavBar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About.</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+NavBar.defaultProps = {
+  icon: "fab fa-github",
+  title: "Github Finder"
+};
+
+NavBar.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default NavBar;
