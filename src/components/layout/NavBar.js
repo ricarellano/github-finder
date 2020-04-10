fdeaf8de58aac86e10756eb58f9b3c2517e44158
Ricardo Arellano <rarellanob@gmail.com>
2020-04-09 23:01:34 -0700

    fix github account

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -13,7 +13,7 @@ const NavBar = ({ icon, title }) => {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">About.</Link>
         </li>
       </ul>
     </nav>
