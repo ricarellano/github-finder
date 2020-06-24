a2b484e97d823cd9e0446452b95a126d52353338
Ricardo Arellano <rarellanob@gmail.com>
2020-06-23 22:23:47 -0700

    added hooks

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,24 +1,20 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment, useEffect, useContext } from 'react'
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-export class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+const User = ({ match}) => {
+  const githubContext = useContext(GithubContext);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired,
-    }
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    //eslint-disable-next-line
+  }, []);
 
-    render() {
+    const {getUser, loading, user, repos, getUserRepos} = githubContext;
+
         const { 
             name,
             company,
@@ -33,9 +29,8 @@ export class User extends Component {
             public_repos,
             public_gists,
             hireable
-         } = this.props.user;
+         } = user;
 
-         const {loading, repos} = this.props;
 
          if(loading) return <Spinner/>
 
@@ -99,7 +94,7 @@ export class User extends Component {
             <Repos repos={repos}/>
           </Fragment>
         );
-    }
 }
+
 
 export default User
